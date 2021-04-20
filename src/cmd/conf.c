3304affa25cc31771a12bbea35b20f4d508f6a19
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-04-20 22:51:16 +0800

    Removed unnecessary constants

--- src/cmd/conf.c
+++ src/cmd/conf.c
@@ -8,16 +8,12 @@ conf(int argc, const char** argv, const unsigned int opts)
 Cache Line Size: %u\n\
 L1 Cache Data Size: %u\n\
 L1 Cache Instruction Size: %u\n\
-L2 Cache Size: %u\n\
-L3 Cache Size: %u\n\
 Max Amount of Open Files: %u\n\
 Number of CPUs: %u\n",
                PAGE_SIZE,
                CACHE_LINE,
                D_CACHE,
                I_CACHE,
-               L2_CACHE_SIZE,
-               L3_CACHE_SIZE,
                OPEN_MAX,
                N_CPU);
 }
