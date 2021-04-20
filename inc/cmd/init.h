b18b24b2fbcabeaf8ed9818581fe51a745f48760
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-04-20 22:41:27 +0800

    Working on init functions and arg parsing

--- inc/cmd/init.h
+++ inc/cmd/init.h
@@ -1,6 +1,8 @@
 #ifndef __INIT_H_
 #define __INIT_H_
 
+#define MAX_PATH 4096
+
 int init(int argc, const char** argv, const unsigned int opts);
 
 #endif // __INIT_H_
--- src/cmd/init.c
+++ src/cmd/init.c
@@ -2,6 +2,12 @@
 #include "unistd.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "unistd.h"
+#include "mem/alloc.h"
+#include "sys/stat.h"
+#include "sys/types.h"
+#include "dirent.h"
+#include "string.h"
 
 /*
  * TODO: Implement Donut init function
@@ -9,5 +15,28 @@
 int
 init(int argc, const char** argv, const unsigned int opts)
 {
+        char* path = xmalloc(256);
+        char* arg = xmalloc(256);
+        strncpy(arg, *argv, 255);
+        printf("Path: %s\n", arg);
+
+        if (*argv)
+                strncpy(path, arg, 255);
+        else
+                *path = '.';
+        printf("Path: %s\n", path);
+
+        /* DIR* d_ptr; */
+        /* struct dirent* dir; */
+
+        /* if (!(d_ptr = opendir("."))) { */
+        /*         printf("Failed to open directory.\n"); */
+        /*         return 1; */
+        /* } */
+
+        /* while ((dir = readdir(d_ptr))) { */
+        /*         printf("File name: %s\n", dir->d_name); */
+        /* } */
+
         return 0;
 }
