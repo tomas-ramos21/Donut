6eefd01d123d6d9c8fe7000646341c07f75cdd00
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-05-02 11:37:02 +0800

    Updated "help" string

--- inc/misc/decorations.h
+++ inc/misc/decorations.h
@@ -4,8 +4,9 @@
 #define DONUT "[Donut] "
 #define HELP_CMD "\nThese are common Donut commands used in various situations:\n \
 \n \
-Start a working area: \n \
+Starting a working area: \n \
 \t - init \t Create an empty Donut container \n \
+\t - chkin \t Commit data to an existing Donut container \n \
 \n \
 Check if your Donut isn't spoiled: \n \
 \t - doctor \t Run all the unit tests to check for issues \n \
