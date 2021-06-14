fe219dd85f4686d04d5068377db4554d77f4f0a5
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-06-14 20:35:05 +0800

    Add *nix util paths

---
+++ inc/const/nix_cmds.h
@@ -0,0 +1,16 @@
+#ifndef NIX_CMDS_H_
+#define NIX_CMDS_H_
+
+/**
+ * @file nix_cmds.h
+ * File containing standard unix commands to be used by Donut. These paths might
+ * need to be updated depending on the OS used.
+ */
+
+/**
+ * @def TOUCH_PATH
+ * Path to the "touch" command provided unix and unix-like OS.
+ */
+#define TOUCH_PATH "/usr/bin/touch"
+
+#endif // NIX_CMDS_H_
