13460582aef06d0e4476772376db07770da3e3e1
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-07-23 11:29:03 +0800

    [Core] Completed data list "file addition"

--- src/core/data-list.c
+++ src/core/data-list.c
@@ -7,6 +7,8 @@
 #define ELEM_PER_PG PAGE_SIZE / DATA_FILE_NAME_SIZE
 
 struct data_list {
+        uint32_t idx;        /**< Next index to be populated */
+        uint32_t pg_cnt;     /**< Current number of first level pages */
         uint32_t elem_l;     /**< Elements lefts */
         uint32_t elem_t;     /**< Maximum array capacity */
         void** pgs;          /**< Allocated Pages */
@@ -27,25 +29,26 @@ test_data_list_init(void)
         int ret = 0;
         struct slabs* slabs = init_slabs();
         struct data_list* buf = init_data_list(slabs);
-        ret |= buf->elem_t;
-        ret |= buf->elem_l;
-        ret |= *((uint8_t*)*(buf->pgs));
-        ret |= !((uintptr_t)buf->slabs);
-        return !ret;
+        ret |= (buf->idx == 0) ? 1 : 0;
+        ret &= (buf->pg_cnt == 0) ? 1 : 0;
+        ret &= (buf->elem_l == 0) ? 1 : 0;
+        ret &= (buf->elem_t == 0) ? 1 : 0;
+        ret &= (buf->pgs == 0x0) ? 1 : 0;
+        ret &= (buf->slabs != 0x0) ? 1 : 0;
+        return ret;
 }
 
 inline static void
 grow_allocation(struct data_list* restrict data)
 {
         /* Grow number of pages suppported */
-        uint32_t cur_pgs = data->elem_t / ELEM_PER_PG;
-        uint32_t new_pgs = (cur_pgs + GROWTH_FACTOR);
+        uint32_t new_pgs = data->pg_cnt + GROWTH_FACTOR;
         void** tmp = alloc_slab(data->slabs, new_pgs * __SIZEOF_POINTER__);
-        memcpy(tmp, data->pgs, cur_pgs * __SIZEOF_POINTER__);
+        memcpy(tmp, data->pgs, data->pg_cnt * __SIZEOF_POINTER__);
 
         /* Allocate new pages */
-        while (cur_pgs < new_pgs)
-                tmp[cur_pgs++] = alloc_slab(data->slabs, PAGE_SIZE);
+        while (data->pg_cnt < new_pgs)
+                tmp[data->pg_cnt++] = alloc_slab(data->slabs, PAGE_SIZE);
 
         /* Update bookkeeping state */
         free_slab(data->slabs, data->pgs);
@@ -60,14 +63,14 @@ add_file_to_list(struct data_list* restrict data, char* f_name)
         if (!data->elem_l)
                 grow_allocation(data);
 
-        /* Get page & element index relative to the page */
-        uint64_t idx = data->elem_t - data->elem_l;
-        uint64_t p_idx = (idx / ELEM_PER_PG) + (idx % ELEM_PER_PG) ? 1 : 0;
-        uint64_t e_idx = (idx  / p_idx) + (idx % p_idx) ? 1 : 0;
+        /* Get relative page & element index */
+        uint64_t p_idx = (data->idx / ELEM_PER_PG) + (data->idx % ELEM_PER_PG) ? 1 : 0;
+        uint64_t e_idx = (data->idx  / p_idx) + (data->idx % p_idx) ? 1 : 0;
         char* elem = ((char*)(data->pgs[p_idx])) + (e_idx * DATA_FILE_NAME_SIZE);
 
-        strncpy(elem, f_name, 31);
+        data->idx++;
         data->elem_l--;
+        strncpy(elem, f_name, 31);
 }
 
 int
@@ -82,6 +85,8 @@ test_add_file_to_data_list(void)
         add_file_to_list(buf, test);
         ret |= (buf->elem_t == (GROWTH_FACTOR * ELEM_PER_PG)) ? 1 : 0;
         ret &= (buf->elem_l == ((GROWTH_FACTOR * ELEM_PER_PG)) - 1) ? 1 : 0;
+        ret &= (buf->idx == 1) ? 1 : 0;
+        ret &= (buf->pg_cnt == 512) ? 1 : 0;
 
         /* Check first item content is correct and starts at the right index */
         char* data = buf->pgs[0];
@@ -91,6 +96,8 @@ test_add_file_to_data_list(void)
         add_file_to_list(buf, test);
         ret &= (buf->elem_t == (GROWTH_FACTOR * ELEM_PER_PG)) ? 1 : 0;
         ret &= (buf->elem_l == ((GROWTH_FACTOR * ELEM_PER_PG)) - 2) ? 1 : 0;
+        ret &= (buf->idx == 2) ? 1 : 0;
+        ret &= (buf->pg_cnt == 512) ? 1 : 0;
 
         /* Check second item content is correct and starts at the right index */
         data = ((char*)buf->pgs[0]) + 1 * 32;
