3b05eaa2e49302fbb7a78da7da3e043e49b786b5
Tomas Ramos <tomas.ramosv21@gmail.com>
2021-08-15 08:15:38 +0000

    Account for ARM builds during Make

--- scripts/sys_info.sh
+++ scripts/sys_info.sh
@@ -34,7 +34,7 @@ get_max_fd()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 MAX_FD=`getconf OPEN_MAX | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 MAX_FD=`getconf -a | grep -i OPEN_MAX | tr -dc '[:digit:]'`
         else
                 MAX_FD=""
@@ -55,7 +55,7 @@ get_page_sz()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 PG_SZ=`sysctl -a | grep -i hw.pagesize: | cut -d' ' -f2 | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 PG_SZ=`getconf -a | grep -i PAGE_SIZE | tr -dc '[:digit:]'`
         else
                 PG_SZ=""
@@ -76,7 +76,7 @@ get_cache_line_sz()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 LN_SZ=`sysctl -a | grep -i hw.cachelinesize | cut -d' ' -f2 | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 LN_SZ=`getconf -a | grep -i LEVEL1_DCACHE_LINESIZE | tr -dc '[:digit:]'`
         else
                 LN_SZ=""
@@ -96,7 +96,7 @@ get_cpu_count()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 CPU_CNT=`sysctl -a | grep -i hw.physicalcpu: | cut -d' ' -f2 | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 SOCKETS=`lscpu | grep -i "Socket(s):" | tr -d [[:blank:]] | cut -d ':' -f 2`
                 CORES=`lscpu | grep -i "Core(s) per socket:" | tr -d [[:blank:]] | cut -d ':' -f 2`
                 CPU_CNT=`expr "$SOCKETS" \* "$CORES"`
@@ -114,7 +114,7 @@ get_icache_sz()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 ICACHE_SZ=`sysctl -a | grep -i hw.l1icachesize | cut -d' ' -f2 | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 ICACHE_SZ=`getconf -a | grep -i LEVEL1_ICACHE_SIZE | tr -dc '[:digit:]'`
         else
                 ICACHE_SZ=""
@@ -135,7 +135,7 @@ get_dcache_sz()
 {
         if [ "$1" = "Darwin" ] || [ "$1" = "FreeBSD" ]; then
                 DCACHE_SZ=`sysctl -a | grep -i hw.l1dcachesize | cut -d' ' -f2 | tr -dc '[:digit:]'`
-        elif [ "$1" = "Linux" ]; then
+	elif [ "$1" = "Linux" ] && [ "$ARCH" != "arm" ]; then
                 DCACHE_SZ=`getconf -a | grep -i LEVEL1_DCACHE_SIZE | tr -dc '[:digit:]'`
         else
                 DCACHE_SZ=""
